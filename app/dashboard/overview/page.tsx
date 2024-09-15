ad272b840740d169e9700146880f2feb526c9fe6
caiiirhel <eyyrhamcairhel@gmail.com>
2024-09-15 23:03:44 +1000

    Add loading.tsx and page.tsx to app/dashboard/overview

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -3,13 +3,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
-  fetchRevenue,
   fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
- 
+
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
@@ -17,7 +15,7 @@ export default async function Page() {
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
- 
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -34,7 +32,7 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        {/* Removed RevenueChart component since revenue data is no longer fetched */}
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
