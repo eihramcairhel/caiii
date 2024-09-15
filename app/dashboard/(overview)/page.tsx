9c3b5ca06e9e351e1e25c122f440264443f3f0ad
caiiirhel <eyyrhamcairhel@gmail.com>
2024-09-16 09:29:52 +1000

    Updated Page component and CardWrapper component with fetchCardData and proper usage of Card component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,9 +3,13 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-import LatestInvoicesSkeleton from '@/app/ui/skeletons/latest-invoices-skeleton'; // Make sure this is correctly imported
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
   const {
@@ -21,6 +25,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
